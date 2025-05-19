268c8470f29b04ebd34c6b9936e4c5d40ee3e854
Mateus <mateus.alexoliveira@gmail.com>
2025-05-19 20:53:51 -0300

    ci yaml

--- migrations/env.py
+++ migrations/env.py
@@ -8,7 +8,6 @@ from sqlalchemy import engine_from_config, pool
 
 # 1) Importa o seu Base e models, para o Alembic saber quais tabelas existem
 from app.devices.db.connection import Base
-from app.devices.db import models
 
 # 3) Obtemos a configuração do Alembic
 config = context.config
